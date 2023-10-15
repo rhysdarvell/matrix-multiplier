108bebfbee9e46b1c1c0b592bd63f4ad88b3913b
Rhys Darvell <s3600111@student.rmit.edu.au>
2023-10-15 21:25:18 +1100

    added hardcoded matrices for testing

---
+++ matrix-multiplier.py
@@ -0,0 +1,14 @@
+def main():
+    print("hello world")
+
+    matrix1 = [[1, 2],
+               [3, 4],
+               [5, 6]]
+    
+    matrix2 = [[7, 8, 9],
+               [10,11,12]]
+    
+    print(matrix1[0][1])
+
+if __name__ == "__main__":
+    main()
